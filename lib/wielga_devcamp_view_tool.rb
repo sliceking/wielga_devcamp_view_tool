3f90a5eae73491009e5034a43b038b6f9edb32b0
stan wielga <svwielga4@gmail.com>
2018-05-25 14:44:59 -0700

    dev tool initial commit

---
+++ lib/wielga_devcamp_view_tool.rb
@@ -0,0 +1,6 @@
+require "wielga_devcamp_view_tool/version"
+require "wielga_devcamp_view_tool/renderer"
+
+module WielgaDevcampViewTool
+  # Your code goes here...
+end
---
+++ lib/wielga_devcamp_view_tool/version.rb
@@ -0,0 +1,3 @@
+module WielgaDevcampViewTool
+  VERSION = "0.1.0"
+end
